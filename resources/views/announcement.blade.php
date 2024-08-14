62733313541627652b2545b7c8975e7542883ca2
sariippp <abdurrahmanasgf88@gmail.com>
2024-08-14 14:51:24 +0700

    [Announcement] Don

--- resources/views/announcement.blade.php
+++ resources/views/announcement.blade.php
@@ -1,54 +1,44 @@
 @extends('layouts.app')
 
-@section('title', 'Announcements')
+@section("title", "Announcments")
 
-@section('content')
-    <Style>
-        @font-face {
-            font-family: 'Montserrat Light';
-            font-style: normal;
-            font-weight: bold;
-            src: local('Montserrat'), url('assets/font/Montserrat-Light.ttf');
+@section("css")
+    <style>
+        :root{
+            --c1: #CB553B;
+            --c2: rgba(228, 173, 173, 0.35);
+            --c3: rgba(208, 153, 98, 70);
+            --c4: rgba(201, 65, 44, 0.85);
+            --montserrat-light : 'Montserrat Light';
+            --montserrat-medium : 'Montserrat Medium';
+            --montserrat-bold : 'Montserrat Bold';
         }
 
-        @font-face {
-            font-family: 'Montserrat Medium';
-            font-style: normal;
-            font-weight: normal;
-            src: local('Montserrat'), url('assets/font/Montserrat-Medium.ttf');
+        p {
+            margin: 0;
         }
 
-        @font-face {
-            font-family: 'Montserrat Bold';
-            font-style: normal;
-            font-weight: normal;
-            src: local('Montserrat'), url('assets/font/Montserrat-Bold.ttf');
+        .monst-bold {
+            font-family: var(--montserrat-bold);
         }
-        .row {
-            margin-right: 0;
-            margin-left: 0;
+        
+        .monst-medium {
+            font-family: var(--montserrat-medium);
         }
-
-        .hr {
-            opacity: 1;
+        
+        .monst-light {
+            font-family: var(--montserrat-light);
         }
 
-        .horizontal-lines:after {
-            content: "";
-            height: 5px;
-            width: 500px;
-            background: #2C56A7;
-            display: inherit;
-            position: absolute;
+        /* Announcement */
+        .announ {
+            color: var(--c4);
         }
+        /* Announcement */
 
-        .horizontal-lines {
-            position: relative;
-        }
-
+        /* Card */
         .card {
             font-family: "Monteserrat", sans-serif;
-            color: #2C56A7;
             letter-spacing: 0px;
             text-align: justify;
         }
@@ -56,6 +46,7 @@
         .card-body {
             transition: all 0.2s ease;
             border-radius: 3px;
+            background-color: #F6DFAD
         }
 
         .card-body:hover {
@@ -63,115 +54,76 @@
             box-shadow: 0 6px 10px rgba(0, 0, 0, .08)
         }
 
-        .card-subtitle {
-            color: #2C56A7;
-        }
-
         .card-text {
             font-family: "Monteserrat Light", sans-serif;
         }
+        /* Card */
 
-        .lighthouse {
-            position: relative;
+        /* Box */
+        .box{
+            width: 18%;
+            background-color: var(--c3);
         }
+        /* Box */
 
-        .image-lighthouse {
-            position: absolute;
-            bottom: 0;
-            z-index: -1;
-            transform: translateX(3rem);
-            right: 0px;
+        /* Garis */
+        .garis {
+            height: 9px;
+            background-color: #CB553B;
         }
+        /* Garis */
     </style>
+@endsection
 
-    <body
-        style="background: url('{{ asset('assets') }}/background/Background Announcement.png') top / cover no-repeat;background-attachment: fixed;">
-        <div class="announcement-page pt-4 px-5">
-
-            {{-- Announcement Title --}}
-            <div class="row">
-                <div class="col" style="max-width:500px;">
-                    <h1 style="margin: 1rem; font-family: Montserrat Bold, sans-serif; color:#2C56A7;">ANNOUNCEMENTS</h1>
-                </div>
-                <div class="col md-auto">
-                    <hr class="horizontal-lines"
-                        style="height:5px;color:#2C56A7;opacity:1;background-color:rgba(255,255,255,0);top:50%;margin:0px;">
-                    </hr>
-                </div>
-            </div>
-
-            {{-- Announcement coloumn --}}
-            <div class="row">
-                <div class="col-md-6" style="height: auto;min-width: 65%;padding-left:1.5rem;">
-                    <!----Announcement body---->
-                    <div class="card"
-                        style="border-style: none;background: rgba(255,255,255,0);padding-bottom:1rem;padding-top:1rem;">
-
-                        <!----Announcement Card 1---->
-                        <div class="card-body" style="background-color: #C3E2F6;margin: 1rem;padding:1.5rem;">
-                            <h4 class="card-title" style="font-family: Montserrat Bold, sans-serif;">Pengumuman Terkait</h4>
-                            <h6 class="card-subtitle mb-2" style="padding-bottom:10px;"> <!-- 00 Month 2022 / 00.00 PM --></h6>
-                            <!----Annoucement Text---->
-                            <p class="card-text">
-                                <!--
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                -->
-                            </p>
-                        </div>
-                        <!----End Annoucement Text---->
-                        <!----End Announcement Card---->
-
-                        <!----Announcement Card 1---->
-                        <div class="card-body" style="background-color: #C3E2F6;margin: 1rem;padding:1.5rem;">
-                            <h4 class="card-title" style="font-family: Montserrat Bold, sans-serif;">Pengumuman Terkait</h4>
-                            <h6 class="card-subtitle mb-2" style="padding-bottom:10px;"><!-- 00 Month 2022 / 00.00 PM --></h6>
-                            <!----Annoucement Text---->
-                            <p class="card-text">
-                                <!--
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                -->
-                            </p>
-                        </div>
-                        <!----End Annoucement Text---->
-                        <!----End Announcement Card---->
-
-                        <!----Announcement Card 1---->
-                        <div class="card-body" style="background-color: #C3E2F6;margin: 1rem;padding:1.5rem;">
-                            <h4 class="card-title" style="font-family: Montserrat Bold, sans-serif;">Pengumuman Terkait</h4>
-                            <h6 class="card-subtitle mb-2" style="padding-bottom:10px;"><!-- 00 Month 2022 / 00.00 PM --></h6>
-                            <!----Annoucement Text---->
-                            <p class="card-text">
-                                <!--
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                -->
-                            </p>
-                        </div>
-                        <!----End Annoucement Text---->
-                        <!----End Announcement Card---->
+@section('content')
+    <div class="d-flex">
+        <div class="d-flex justify-content-center align-items-center flex-column pt-5 w-75">
+            <div class="w-100 ps-5 pe-4">
+                <div class="wrap-all">
+                    <h1 class="monst-bold announ">ANNOUNCEMENTS</h1>
+                    <!----Announcement Card 1---->
+                    <div class="card-body" style="margin: 1rem;padding:1.5rem;">
+                        <h4 class="card-title" style="font-family: Montserrat Bold, sans-serif;">Announcement 1</h4>
+                        <h6 class="card-subtitle mb-2" style="padding-bottom:10px;"> 21 September 2024 / Day 1 / 07.30-21.00 </h6>
+                        <!----Annoucement Text---->
+                        <p class="card-text">
+                            Setiap tim akan melwati babak penyisihan dimulai dari Tugu Pahlawan, Kota Lama & Universitas Surabaya bersama dengan KIKO the Red Panda.
+                        </p>
                     </div>
-                    <!----End Announcement Body---->
+                    <!----End Annoucement Text---->
+                    <!----End Announcement Card---->
+
+                    <!----Announcement Card 1---->
+                    <div class="card-body" style="margin: 1rem;padding:1.5rem;">
+                        <h4 class="card-title" style="font-family: Montserrat Bold, sans-serif;">Announcement 2</h4>
+                        <h6 class="card-subtitle mb-2" style="padding-bottom:10px;"> 22 September 2024 / Day 2 / 09.00 - 20.00 </h6>
+                        <!----Annoucement Text---->
+                        <p class="card-text">
+                            Bagi tim yang lolos babak penyisihan, akan melewati babak FINAL yang akan di laksanakan di Universitas Surabaya. Tetap semangat untuk setiap tim !
+                        </p>
+                    </div>
+                    <!----End Annoucement Text---->
+                    <!----End Announcement Card---->
+
+                    <!----Announcement Card 1---->
+                    <div class="card-body" style="margin: 1rem;padding:1.5rem;">
+                        <h4 class="card-title" style="font-family: Montserrat Bold, sans-serif;">Announcement 3</h4>
+                        <h6 class="card-subtitle mb-2" style="padding-bottom:10px;"> 22 September / Day 2 / 17.00 - 20.00 </h6>
+                        <!----Annoucement Text---->
+                        <p class="card-text">
+                            Akan dilaksanakan closing ceremony yang akan di datangi oleh peserta lomba Video TikTok & 10 tim yang lolos pada babak FINAL. Sampai Jumpa di Closing Ceremony !
+                        </p>
+                    </div>
+                    <!----End Annoucement Text---->
+                    <!----End Announcement Card---->
                 </div>
             </div>
-
-            {{-- Sponsor
-            @include('layouts.sponsor')
-            <div class="row spacing-bawah"></div> --}}
-
-            {{-- Maskot --}}
-            <div class="lighthouse">
-                <img class="image-lighthouse" src="{{ asset('assets') }}/img/Lighthouse Announcement.png" />
-            </div>
-    </body>
+        </div>
+        <div class="w-25 overflow-y-hidden d-flex justify-content-around">
+            <div class="box1 box h-100"></div>
+            <div class="box2 box h-100"></div>
+            <div class="box3 box h-100"></div>
+        </div>
+    </div>
+    <div class="garis w-100"></div>
 @endsection
